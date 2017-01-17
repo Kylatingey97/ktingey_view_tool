b787f58e3603b3b4b84601ead1d7029ab7d97e47
Kyla  Tingey <kylatingey@Kylas-MacBook-Pro.local>
2017-01-17 11:07:59 -0700

    Updated gitignore file and readme

---
+++ lib/ktingey_view_tool.rb
@@ -0,0 +1,6 @@
+require "ktingey_view_tool/version"
+require "ktingey_view_tool/renderer"
+
+module KtingeyViewTool
+  # Your code goes here...
+end
---
+++ lib/ktingey_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KtingeyViewTool
+  VERSION = "0.1.0"
+end
